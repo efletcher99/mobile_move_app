366fbfcca8092cc71b0969830ab1febea8e0eb4d
Ethan Fletcher <ethanfletch99@gmail.com>
2025-06-24 17:04:07 -0600

    added search bar

---
+++ services/api.ts
@@ -0,0 +1,15 @@
+
+
+/*const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2NWFjYWVlYTVkMWU3ZGVkZTE1MDc1MDJjOWEzOWFlZCIsIm5iZiI6MTc1MDgwNTQ5NC41MDMsInN1YiI6IjY4NWIyYmY2ZGMwMTlkMmFhZWFmYTM3OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.zfijiMG2D5E3Xwpbu-hLx2HOlGB9DF66jl0KcnoApOA'
+  }
+};
+
+fetch(url, options)
+  .then(res => res.json())
+  .then(json => console.log(json))
+  .catch(err => console.error(err));*/
